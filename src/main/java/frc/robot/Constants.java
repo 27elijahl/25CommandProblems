a8f811fb428c8a2516c5b26945a4f793a8478387
27elijahl <27elijahl@students.harker.org>
2025-07-22 14:54:51 -0700

    Added constants in Constants.java

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class Constants 
+{
+    public class Elevator
+    {
+        // heights, in meters, of reaching any of the levels and
+        // intaking a coral
+        public static final double LEVEL1_HEIGHT = 0.5;
+        public static final double LEVEL2_HEIGHT = 1.0;
+        public static final double LEVEL3_HEIGHT = 1.5;
+        public static final double LEVEL4_HEIGHT = 2.0;
+        public static final double INTAKE_HEIGHT = 0.75;
+    }
+}
